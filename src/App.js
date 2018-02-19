7e258561392208106dcdd6370195468a5df07f1d
saivicky2015 <saivicky2015@gmail.com>
2018-02-19 19:17:57 +0530

    final commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+import SearchBar from './containers/search_bar';
+
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <SearchBar/>
+
+      </div>
+    );
+  }
+}
+
+
+
+export default App;
+ 
